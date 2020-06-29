a1ae91b6a8ff15ea3378f2f1400286a022de27dd
KalingKarlsson <46886642+KalingKarlsson@users.noreply.github.com>
2020-06-29 13:15:16 +0200

    inital commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const http = require('http')
+const fs = require('fs')
+const express = require('express')
+const { response } = require('express')
+const app = express()
+
+app.get('/hello',function(request, response){
+    response.send("Hello")
+})
+
+
+/*
+const server = http.createServer(function(req, res){
+
+    if(req.method == "GET"){
+        "/home.html"
+        const filename = req.url.substr(1)
+        fs.readFile("pages/"+filename, "utf8", function(err, content){
+            if(err){
+                res.statusCode = 404
+                res.setHeader("Conntent-type", "text/plain")
+                res.end("<h1>not error</h1><p>...</p>")
+            }else{
+                res.statusCode = 200
+                res.setHeader("Conntent-type", "text/plain")
+                res.end(content)
+            }
+        })
+    }
+    else{
+        res.statusCode = 404
+        res.setHeader("Conntent-type", "text/plain")
+        res.end("<h1>BAD</h1><p>Hej</p>")
+    }
+})*/
+
+server.listen(8080)
