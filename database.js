373ab6d11dac715df5fb471c6454d4cef45a837d
lubbe <ludwigjareb@gmail.com>
2020-07-01 12:10:57 +0200

    Db-Anton

--- database.js
+++ database.js
@@ -7,6 +7,58 @@ const databaseConnection = mysql.createConnection({
 	database: "antludsDatabase1"
 });
 
-databaseConnection.connect(function(err){
-	if (err) throw err;
+databaseConnection.connect(function (err) {
+	if (err) {
+		throw err;
+	} else {
+		console.log('connected to DB was successful')
+	}
 })
+
+function createUserTable() {
+	const tableQuery = `CREATE TABLE IF NOT EXISTS Users (
+		UserID INT AUTO_INCREMENT PRIMARY KEY, 
+		Username VARCHAR(25) NOT NULL UNIQUE,
+		Password VARCHAR(25) NOT NULL
+		)`;
+
+	databaseConnection.query(tableQuery, function (error, result) {
+		if (error) {
+			throw error;
+		}
+	})
+}
+
+function createImageTable() {
+	const imagesTableQuery = `CREATE TABLE IF NOT EXISTS Images (
+		ImgID INT AUTO_INCREMENT PRIMARY KEY,
+		OwnerID INT NOT NULL,
+		ImgName VARCHAR(25) NOT NULL,
+		Caption TEXT,
+		FOREIGN KEY (OwnerID) REFERENCES Users(UserID) ON DELETE CASCADE	
+	)`;
+
+	databaseConnection.query(imagesTableQuery, function (error, result) {
+		if (error) {
+			throw error;
+		}
+	})
+}
+
+function createLikerTable() {
+	const likerTableQuery = `CREATE TABLE IF NOT EXISTS Liker (
+		LikerID INT NOT NULL,
+		ImgID INT NOT NULL,
+        FOREIGN KEY (ImgID) REFERENCES Images(ImgID) ON DELETE CASCADE,
+        FOREIGN KEY (LikerID) REFERENCES Users(UserID) ON DELETE CASCADE
+	)`;
+
+	databaseConnection.query(likerTableQuery, function (error, result) {
+		if (error) {
+			throw error;
+		}
+	});
+}
+createUserTable();
+createImageTable();
+createLikerTable();
