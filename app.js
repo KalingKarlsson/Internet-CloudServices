0e12f243155e5ab1e59181cc49574574f0ce4bee
lubbe <ludwigjareb@gmail.com>
2020-07-15 09:50:50 +0200

    Update

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const credentials = new AWS.Credentials({
     accessKeyID: 'AKIAJD7EZCOE2DQJTNWQ',
     secretAccessKey:'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'
 })
+
 const s3 = new AWS.S3({
     credentials: credentials,
     region: 'eu-west-1b'
@@ -35,6 +36,7 @@ app.post("/login", function(request, response){
         return
     }
 
+    
     db.getUserWithUsername(username, function(error, users){
         if(error){
             response.status(500).end()
@@ -63,5 +65,86 @@ app.post("/login", function(request, response){
 
 })
 
+/*
+app.get('/addpost1', (req , res) => {
+    let post = {UserID: 'Post one', username:'This is a post'};
+    let sql = 'INSERT INTO Users SET ?';
+    let query = db.query(sql,post,(err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post 1');
+    });
+});
+*/
+
+const users = [{
+    id: 1,
+    name: "Ludwig"
+},
+ {
+    id: 2,
+    name: "Anton"
+}, 
+{
+    id: 3,
+    name: "PLG"
+}]
+
+const bodyParser = require('body-parser')
+const {
+    HttpRequest,
+    FSx
+} = require('aws-sdk')
+app.use(bodyParser.json())
+app.post('/users', function (request, response) {
+    const newUser = request.body
+    newUser.userID = users[users.length - 1].userID + 1
+    users.push(newUser)
+    response.setHeader("Location", "/users/" + newUser.userID)
+    response.status(201).end()
+})
+
+
+app.get('/users', function (request, response) {
+    response.status(200).json(users)
+})
+
+app.get('/users/:id', function (request, response) {
+    const id = parseInt(request.params.userID)
+    const user = users.find(users => users.userID == id)
+    if (user) {
+        const id = request.params.id;
+        response.status(200).json(user)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.put('/users/:id', function (request, response) {
+    const id = parseInt(request.params.UserID)
+    const updatedUser = request.body
+    const index = users.findIndex(users => users.userID == id)
+    if (index == -1) {
+        response.status(404).end()
+    } else {
+        users[index] = updatedUser
+        response.status(204).end()
+    }
+})
+
+/*
 var port = process.env.PORT || 8080
 server.listen(port)
+*/
+
+app.get('/', function (request, response) {
+    response.sendfile("home.html")
+})
+
+app.get('upload/:itemID', function (request, response) {
+    console.log("FIle sent!");
+})
+
+app.listen("8080", () => {
+  console.log("Server Started on port 8000");
+});
--- database.js
+++ database.js
@@ -6,6 +6,7 @@ const databaseConnection = mysql.createConnection({
   password: "antlud123",
   database: "antludsDatabase1",
 });
+
 // Tables
 databaseConnection.connect(function (err) {
   if (err) {
@@ -59,137 +60,7 @@ function createLikerTable() {
     }
   });
 }
-
-//User info
-exports.createUser = function (newUser, callback) {
-  const query = "INSERT INTO Users (Username, Password) VALUES (?,?)";
-  const values = [newUser.username, newUser.password];
-  databaseConnection.query(query, values, function (error, user) {
-    callback(error, user);
-  });
-};
-exports.getUserWithID = function (id, callback) {
-  const query = "SELECT * FROM Users WHERE UserID = ?";
-  databaseConnection.query(query, id, function (error, user) {
-    callback(error, user);
-  });
-};
-exports.getUserWithUsername = function (username, callback) {
-  const query = "SELECT * FROM Users WHERE Username = ?";
-  databaseConnection.query(query, username, function (error, user) {
-    callback(error, user);
-  });
-};
-exports.changeUserPassword = function (user, callback) {
-  const query = "UPDATE Users SET Password = ? WHERE UserID = ?";
-  const values = [user.password, user.userID];
-  databaseConnection.query(query, values, function (error, user) {
-    callback(error, user);
-  });
-};
-exports.deleteUser = function (ID, callback) {
-  const query = "DELETE FROM Users WHERE UserID = ?";
-  databaseConnection.query(query, ID, function (error, deletion) {
-    callback(error, deletion);
-  });
-};
-
-//Images
-exports.createImage = function (newImage, callback) {
-  const query = "INSERT INTO Images (OwnerID, ImgName, Caption) VALUES (?,?,?)";
-  const values = [newImage.ownerID, newImage.imgName, newImage.Caption];
-  databaseConnection.query(query, values, function (error, image) {
-    callback(error, image);
-  });
-};
-exports.getImageWithId = function (id, callback) {
-  const query = "SELECT * FROM Images WHERE ImgID = ?";
-  databaseConnection.query(query, id, function (error, image) {
-    callback(error, image);
-  });
-};
-exports.changeImage = function (image, callback) {
-  const query = "UPDATE Images SET  imgName = ?, Caption = ? WHERE ImgID = ?";
-  const values = [image.imgName, image.Caption, image.imgID];
-  databaseConnection.query(query, values, function (error, alteredImg) {
-    callback(error, alteredImg);
-  });
-};
-exports.deleteImage = function (ID, callback) {
-  const query = "DELETE FROM Images WHERE ImgID = ?";
-  databaseConnection.query(query, ID, function (error, deletion) {
-    callback(error, deletion);
-  });
-};
-exports.getOwnerIdFromImage = function (id, callback) {
-  const query = "SELECT * FROM Images WHERE ImgID = ?";
-  databaseConnection.query(query, id, function (error, image) {
-    callback(error, image[0].OwnerID);
-  });
-};
-/*//Get all valuables owned by a user
-exports.getUsersItems = function(id, callback){
-	const query = "SELECT * FROM Valuables WHERE OwnerID = ?"
-	databaseConnection.query(query,id,function(error, vals){
-		callback(error, vals)
-	})
-}*/
-
-//Admin api
-exports.changeDB = function (query, callback) {
-  databaseConnection.query(query, function (error, resp) {
-    callback(error, resp);
-  });
-};
-
-//Return all users in database
-exports.getAllUsers = function (callback) {
-  databaseConnection.query("SELECT * FROM Users", function (error, users) {
-    callback(error, users);
-  });
-};
-
-//Return all Images in database
-exports.getAllImages = function (callback) {
-  databaseConnection.query("SELECT * FROM Images", function (error, images) {
-    callback(error, images);
-  });
-};
-
-/*
-exports.connectWarrantyToValuable = function(warID, valID, callback){
-	const firstQuery = "UPDATE Valuables SET WarrantyID = null WHERE WarrantyID = ?"
-	databaseConnection.query(firstQuery, warID,function(error, deletion){
-		if(!error){
-			const query = "UPDATE Valuables SET WarrantyID = ? WHERE ItemID = ?"
-			const values = [warID, valID]
-			databaseConnection.query(query, values,function(error, change){
-			callback(error, change)
-			})
-		}
-		else{
-			callback(error, deletion)	
-		}
-	})
-	
-}
-exports.updateItemCount = function(userID, callback){
-	const query = "UPDATE Users SET ItemCount = (SELECT COUNT(OwnerID) FROM Valuables WHERE OwnerID = ?) WHERE UserID = ?"
-	const ID = [userID, userID]
-	databaseConnection.query(query, values, function(error, update){
-		callback(error, update)
-	})
-}
-
-exports.connectFileToValuable = function(fileName, itemID, callback){
-	const query = "UPDATE Valuables SET Photo = ? WHERE ItemID = ?"
-	const values = [fileName,itemID]
-	databaseConnection.query(query, values, function(error, resp){
-		callback(error, resp)
-	})
-}
-
-*/
+ 
 
 createUserTable();
 createImageTable();
